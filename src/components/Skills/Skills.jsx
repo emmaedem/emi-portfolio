201a94420021314e00b2e2067a867eb6e610d745
emmaedem <emmaontop86@yahoo.com>
2024-11-26 00:38:05 +0100

    modified:   src/components/Skills/Skills.jsx

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,8 +1,32 @@
 import styles from './Skills.module.css';
+import {getImageUrl} from "../../utils.js";
 
-const Skills = () => {
+
+ const Skills = () => {
     return (
-        <section className={styles.Container} id="Work"> 
+        <section className={styles.Container} id="Work">
+            <h2>Experince</h2>
+            <div>
+                <div>
+                    {Skills.map((skill, id) => {
+                        return (
+                            <div key={id}>
+                                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                                <p>{skill.title}</p>
+                            </div>
+                        )
+                    
+                })};
+                    
+
+                </div>
+                <ul>
+
+                </ul>
+
+            </div>
+
+            
             
             
             
