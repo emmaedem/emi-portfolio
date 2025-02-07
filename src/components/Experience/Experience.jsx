ec674a2742da93eba949c3f513de4e83e872dfc3
emmaedem <emmaontop86@yahoo.com>
2025-02-07 01:48:12 +0100

    modified:   src/components/Experience/Experience.jsx 	renamed:    src/components/data/history.json -> src/data/history.json 	renamed:    src/components/data/skills.json -> src/data/skills.json

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,38 +1,26 @@
 import styles from './Experience.module.css';
 import { getImageUrl } from "../../utils.js";
-import skills from "../data/skills.json";
+import skills from "../../data/skills.json";
 
-
- const Experience = () => {
+const Experience = () => {
     return (
         <section className={styles.Container} id="experience">
             <h2>Experience</h2>
             <div>
-                <div>
-                    {skills.map((skill, id) => {
-                        return (
-                            <div key={id}>
+                {skills.map((skill, id) => {
+                    return (
+                        <div key={id}>
+                            <div>
                                 <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
                                 <p>{skill.title}</p>
                             </div>
-                        )
-                    
-                })}
-                    
-
-                </div>
-                <ul>
-
-                </ul>
-
+                        </div>
+                    );
+                })};
+                
             </div>
-
-    
-           
-      
         </section>
-    
-    )
-}
+    );
+};
 
-export default Experience
+export default Experience;
